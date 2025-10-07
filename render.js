93c9e3b75a534fa3a361337928595c22f53b9417
mustafa-bhatti <95363241+mustafa-bhatti@users.noreply.github.com>
2025-10-07 17:26:06 +0500

    Added a default value for the "to" currency in the fetchCurrencies function call.

--- render.js
+++ render.js
@@ -9,8 +9,12 @@ const renderCurrNames = async function () {
     const option = document.createElement('option');
     option.value = currentCurrency;
     option.textContent = currentCurrency;
+    if (currentCurrency == 'USD') {
+      option.selected = true;
+    }
+    const option2 = option.cloneNode(true);
     fromCurrency.appendChild(option);
-    toCurrency.appendChild(option);
+    toCurrency.appendChild(option2);
   });
 };
 export { renderCurrNames };
